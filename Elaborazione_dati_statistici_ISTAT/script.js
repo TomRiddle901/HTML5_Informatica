ce9d7c5426b7b06a20a5ee29420f02ab0d4fca0c
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-22 15:13:00 +0100

    Refactor category selection in main HTML and script.js for improved data handling

--- Elaborazione_dati_statistici_ISTAT/script.js
+++ Elaborazione_dati_statistici_ISTAT/script.js
@@ -1,18 +1,16 @@
 var categorie = [];
-var sottocategorie = [];
 
 function caricaJson(){
     fetch('paniere_istat.json')
     .then(r => r.json())
     .then(data => {
         categorie = data.categorie;
-        sottocategorie = data.sottocategorie;
         popolaSelectCategorie();
     })
 }
 
 function popolaSelectCategorie(){
-    var selectCategoria = document.getElementById("categoria");
+    var selectCategoria = document.getElementById("categoria_select");
     selectCategoria.innerHTML = "";
 
     var optDefault = document.createElement("option");
@@ -23,7 +21,7 @@ function popolaSelectCategorie(){
     for (var i = 0; i < categorie.length; i++){
         var elOption = document.createElement("option");
         elOption.text = categorie[i].nome;
-        elOption.value = categorie[i].nome;
+        elOption.value = i.toString();
         selectCategoria.add(elOption);
     }
 }
